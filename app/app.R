448da51c89b111794ead5ea8ce53ba2430666f2f
Antti Rask <anttilennartrask@gmail.com>
2025-04-05 02:44:45 +0300

    Debugging, once more

--- app/app.R
+++ app/app.R
@@ -1,16 +1,23 @@
-cat("Starting app...\n")
-tryCatch(
-  {
-    source("ui.R")
-    cat("Loaded ui.R\n")
-    source("server.R")
-    cat("Loaded server.R\n")
-    shinyApp(ui = ui, server = server)
-  },
-  error = function(e) {
-    cat("CRASH DURING INIT:\n")
-    cat(conditionMessage(e), "\n")
-    traceback()
-    stop("App failed to start")
+# cat("Starting app...\n")
+# tryCatch(
+#   {
+#     source("ui.R")
+#     cat("Loaded ui.R\n")
+#     source("server.R")
+#     cat("Loaded server.R\n")
+#     shinyApp(ui = ui, server = server)
+#   },
+#   error = function(e) {
+#     cat("CRASH DURING INIT:\n")
+#     cat(conditionMessage(e), "\n")
+#     traceback()
+#     stop("App failed to start")
+#   }
+# )
+
+library(shiny)
+shinyApp(
+  ui = fluidPage("Shiny works on Fly!"),
+  server = function(input, output, session) {
   }
 )
